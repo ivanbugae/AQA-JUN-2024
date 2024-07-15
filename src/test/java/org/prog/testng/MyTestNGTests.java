185b3cf5c9bb5273ec5e09ea4b84a49d9e3fa292
ivanbugae <bugaevivan22@gmail.com>
2024-07-15 18:53:37 +0300

    Home Work 7

--- src/test/java/org/prog/testng/MyTestNGTests.java
+++ src/test/java/org/prog/testng/MyTestNGTests.java
@@ -1,29 +1,29 @@
 package org.prog.testng;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.prog.driver.WedDriverFactory;
+import org.prog.page.AlloPage;
 import org.prog.page.GooglePage;
 import org.prog.page.ImdbPage;
 import org.prog.page.WikiPage;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class MyTestNGTests {
+public class  MyTestNGTests {
 
 
     private WebDriver driver;
 
-    private GooglePage googlePage;
-    private WikiPage wikiPage;
-    private ImdbPage imdbPage;
+    private AlloPage alloPage;
+
 
     @BeforeSuite
     public void setUp() {
         driver = WedDriverFactory.getDriver();
-        googlePage = new GooglePage(driver);
-        wikiPage = new WikiPage(driver);
-        imdbPage = new ImdbPage(driver);
+        alloPage = new AlloPage(driver);
+
     }
 
     @AfterSuite
@@ -35,51 +35,52 @@ public class MyTestNGTests {
 
     @BeforeMethod
     public void reloadGooglePage() {
-        googlePage.loadPage();
-        googlePage.acceptCookieIfPresent();
+        alloPage.loadPage();
     }
 
     //google-dev.com
     //google-it.com
     //google-preprod.com
 
-    @Test(dataProvider = "celebrityNames")
-    public void mtNGTest1(String celebrityName, String additionalInfo) {
+    @Test(dataProvider = "iphone")
+    public void mtNGTest1(String iphone, String additionalInfo) {
         System.out.println(additionalInfo);
 
-        googlePage.setSearchValue(celebrityName);
-        googlePage.executeSearch();
+        alloPage.setSearchValue(iphone);
+        alloPage.executeSearch();
 
         int searchCounter = 0;
-        for (WebElement header : googlePage.getSearchHeaders()) {
-            if (header.getText().toUpperCase().contains(celebrityName.toUpperCase())) {
+        for (WebElement header : alloPage.getSearchHeaders()) {
+            if (header.getText().toUpperCase().contains(iphone.toUpperCase())) {
                 searchCounter++;
             }
-        }
 
+
+        }
+        //System.out.println(searchCounter);
         Assert.assertTrue(searchCounter > 2,
                 "Search count of google results expected to be more than 2, but was " + searchCounter);
-    }
 
-    @Test(dataProvider = "celebrityNames")
-    public void feelingLuckyTest(String celebrityName, String additionalInfo) {
-        System.out.println(additionalInfo);
 
-        googlePage.setSearchValue(celebrityName);
-        googlePage.feelingLucky();
 
-        if ("Ben Affleck".equals(celebrityName)) {
-            Assert.assertTrue(wikiPage.getPageUrl().contains("wiki"));
-        } else {
-            Assert.assertTrue(imdbPage.getPageUrl().contains("imdb"));
-        }
+                String price = driver.findElements(By.className("sum")).get(1).getText();
+               // System.out.println(price);
+            driver.findElements(By.className("product-card__title")).get(0).click();
+            String price1 = driver.findElements(By.className("sum")).get(1).getText();
+          //  System.out.println(price1);
+            if(price.equals(price1)){
+                System.out.println(price+"="+price1);
+            }
+
+
+
+
     }
 
     @DataProvider
-    public Object[][] celebrityNames() {
+    public Object[][] iphone() {
         return new Object[][]{
-                {"Ben Affleck", "Looking for Ben"},
-                {"Margot Robbie", "Looking for Margot"}
+                {"iPhone", "Looking for iPhone"}
         };
     }
 }
